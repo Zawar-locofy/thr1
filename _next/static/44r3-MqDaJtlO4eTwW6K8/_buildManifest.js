bd8676074f15e6a512498b44002bc3ed7cb23dc9
Zawar-locofy <>
2023-08-27 17:37:32 +0000

    Updates

--- _next/static/44r3-MqDaJtlO4eTwW6K8/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/547-22655930fd496bc9.js","static/css/f874ccebf62a27b4.css","static/chunks/pages/index-b727024d76411881.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
